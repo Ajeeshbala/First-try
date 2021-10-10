5bd991737e387743575fc5ec6049cfd8c3a16d92
Ajeeshbala <ajeeshbala129@gmail.com>
2021-10-10 21:10:02 +0530

    balaji

---
+++ src/org/base/Sample.java
@@ -0,0 +1,81 @@
+package org.base;
+
+public class Sample {
+
+	public static void main(String[] args) {
+
+		//reverse Sring
+
+		//	String s="java";
+		//	String rev =" ";
+		//	int length = s.length();
+		//	System.out.println(length);
+		//	for (int i = s.length()-1; i >=0; i--) {
+		//		char charAt = s.charAt(i);
+		//
+		//		rev=rev+charAt;
+		//	}
+		//	System.out.println(rev);
+		//	
+
+
+
+		//palindrome
+
+//		String s ="malayalam";
+//		String rev ="";
+//		int length = s.length();
+//		System.out.println(length);
+//		for (int i =s.length()-1; i >=0 ; i--) {
+//			char charAt = s.charAt(i);
+//			rev=rev+charAt;
+//
+//		}
+//		System.out.println(rev);
+//		if (s.equals(rev)) {
+//			System.out.println("palindrome");
+//
+//		}else
+//			System.out.println("not palindrome");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
